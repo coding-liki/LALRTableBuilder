17f98847e3c682b8690e32751dc8ae9cd50e4023
Лев Некрасов <lev.nekrasov@devim.team>
2021-04-14 16:43:14 +0300

    Добавил в инит скрипт построение и сохранение таблицы

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
 declare(strict_types=1);
 
 use CodingLiki\GrammarParser\GrammarRuleParser;
+use LALR1Automaton\AutomatonBuilder;
+use LALR1Automaton\Table\LALR1TableBuilder;
 
-include_once __DIR__."/../vendor/CodingLiki/OoAutoloader/Autoloader.php";
+include_once __DIR__ . "/../vendor/CodingLiki/OoAutoloader/Autoloader.php";
 
-if($argc < 2){
+if ($argc < 2) {
     echo "Недостаточно аргументов.
 Укажите путь к файлу грамматик!
 ";
@@ -15,12 +17,21 @@ if($argc < 2){
 }
 $path = $argv[1];
 
-if(!file_exists($path)){
+if (!file_exists($path)) {
     echo "Указанный файл ($path) не существует.
 ";
     exit(1);
 }
 $rules = GrammarRuleParser::parse(file_get_contents($path));
+$builder = new AutomatonBuilder($rules);
+$state = $builder->buildFromRules();
 
-print_r($rules);
+$tableBuilder = new LALR1TableBuilder($state, $builder->rules);
+
+$table = $tableBuilder->build();
+
+$saver = new \LALR1Automaton\Table\CsvSaver($table);
+
+$saver->save(dirname($path) . "/" . basename($path) . ".lrt");
+
 __HALT_COMPILER();
